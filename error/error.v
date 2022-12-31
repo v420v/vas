42d7e0fc1b409fc28c396f66c6a406246cf9b38b
v420v <ibuki420v@gmail.com>
2023-01-01 03:32:53 +0900

    Update error message

--- error/error.v
+++ error/error.v
@@ -16,6 +16,12 @@ pub fn new_error(pos token.Position, msg string) Vas_Error {
 	}
 }
 
+fn print_space(n int) {
+	for _ in 0 .. n {
+		eprint(' ')
+	}
+}
+
 pub fn print(e Vas_Error) {
 	program := os.read_file(e.pos.file_name) or {
 		eprintln('error: reading file `$e.pos.file_name`')
@@ -28,16 +34,19 @@ pub fn print(e Vas_Error) {
 
 	eprintln('\u001b[1m$e.pos.file_name:$e.pos.line:$e.pos.col: \x1b[91merror\x1b[0m\u001b[1m: $e.msg \033[0m')
 
+	print_space(e.pos.line.str().len + 2)
+	eprintln('|')
+
 	eprintln(' $e.pos.line | $code')
 
-	for _ in 0 .. e.pos.col + 4 + e.pos.line.str().len {
-		eprint(' ')
-	}
+	print_space(e.pos.line.str().len + 2)
+	eprint('|')
+
+	print_space(e.pos.col + 1)
 
 	for _ in 0 .. e.pos.len {
 		eprint('\u001b[1m\x1b[91m~\033[0m')
 	}
-
 	eprintln('')
 }
 
