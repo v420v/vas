20bd7fdced6d16247142765b1f4d0cb1b5c0c3fc
v420v <ibuki420v@gmail.com>
2023-03-01 09:51:04 +0900

    loop example using cmp and jmp

---
+++ examples/loop.s
@@ -0,0 +1,49 @@
+# ./vas -o loop.o loop.s
+# ld -o loop loop.o
+# ./loop
+# > Hello Hello Hello Hello Hello
+
+.global _start
+
+.data
+msg:
+	.string "Hello "
+
+.text
+_start:
+	pushq %rbp
+	movq %rsp, %rbp
+	subq $16, %rsp
+
+	# int a = 0
+	movq $0, %rax
+	movq %rax, 0-4(%rbp)
+
+loop_start:
+	movq 0-4(%rbp), %rdi
+	movq $5, %rax
+
+	# a == 5 -> jmp loop_end
+	cmp %rdi, %rax
+	je loop_end
+
+	# print msg
+	movq $1, %rax
+	movq $1, %rdi
+	leaq msg(%rip), %rsi
+	movq $7, %rdx
+	syscall
+
+	# a = a + 1
+	movq 0-4(%rbp), %rax
+	addq $1, %rax
+	movq %rax, 0-4(%rbp)
+
+	# jmp loop_start
+	jmp loop_start
+
+loop_end:
+	movq $60, %rax
+	movq $0, %rdi
+	syscall
+
